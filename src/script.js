a8118d62790a01a33d7c3d0cb4bbd00e2d67d066
Mattamosa <121215031+Mattamosa@users.noreply.github.com>
2024-12-10 00:50:14 -0700

    Added an empty archive modal

--- src/script.js
+++ src/script.js
@@ -373,7 +373,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
       if (response.ok) {
         console.log('Archived entries deleted:', result.message);
-        alert('All archived entries have been successfully deleted!');
+        // alert('All archived entries have been successfully deleted!');
         const currentPage = window.location.pathname.split("/").pop();
         if (currentPage === "all-entries.html") {
           fetchEntries("archived"); // Refresh the displayed entries with the archived filter
@@ -439,4 +439,26 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   fetchAnalytics();
+  // Empty Archive Modal
+  window.openModal = function(modalId) {
+    document.getElementById(modalId).style.display = 'block'
+    document.getElementsByTagName('body')[0].classList.add('overflow-y-hidden')
+}
+
+window.closeModal = function(modalId) {
+    document.getElementById(modalId).style.display = 'none'
+    document.getElementsByTagName('body')[0].classList.remove('overflow-y-hidden')
+}
+
+// Close all modals when press ESC
+document.onkeydown = function(event) {
+    event = event || window.event;
+    if (event.keyCode === 27) {
+        document.getElementsByTagName('body')[0].classList.remove('overflow-y-hidden')
+        let modals = document.getElementsByClassName('modal');
+        Array.prototype.slice.call(modals).forEach(i => {
+            i.style.display = 'none'
+        })
+    }
+};
 });
